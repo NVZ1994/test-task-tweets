33e71b9e0ec9dad5b250fc3c686c2a32a28727a7
NVZ1994 <110624272+NVZ1994@users.noreply.github.com>
2023-05-09 22:24:31 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter  } from "react-router-dom";
+import { BrowserRouter   } from "react-router-dom";
 
 import App from "components/App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HashRouter  basename="/test-task-tweets">
+    <BrowserRouter basename="/test-task-tweets">
       <App />
-    </HashRouter >
+    </BrowserRouter  >
   </React.StrictMode>
 );
